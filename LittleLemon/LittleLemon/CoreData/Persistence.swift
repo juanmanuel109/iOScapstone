b699d37b6c39ecf361535ecbe88d7c3a127f4835
juanmanuel109 <juanmanuelordonez02@icloud.com>
2024-06-23 22:51:37 -0500

    update june 23 night

--- LittleLemon/LittleLemon/CoreData/Persistence.swift
+++ LittleLemon/LittleLemon/CoreData/Persistence.swift
@@ -10,14 +10,25 @@ import Foundation
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-
+    
+    
     let container: NSPersistentContainer
-
+    
     init() {
-        container = NSPersistentContainer(name: "ExampleDatabase")
+        container = NSPersistentContainer(name: "LittleLemon")
         container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         container.loadPersistentStores(completionHandler: {_,_ in})
+        container.viewContext.automaticallyMergesChangesFromParent = true
     }
-      
+    func clear() {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Dish")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        let _ = try? container.persistentStoreCoordinator.execute(deleteRequest, with: container.viewContext)
+    }
+    
+    static func oneDish() -> Dish {
+        let dish = Dish(context: shared.container.viewContext)
+        
+        return dish
+    }
 }
