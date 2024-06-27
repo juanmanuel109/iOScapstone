8f61bfc131c7560052d1ba97a3535a9d446b04ab
juanmanuel109 <juanmanuelordonez02@icloud.com>
2024-06-26 20:14:10 -0500

    update june 26 night

--- LittleLemon/LittleLemon/Views/DetailItem.swift
+++ LittleLemon/LittleLemon/Views/DetailItem.swift
@@ -5,4 +5,42 @@
 //  Created by Juan Manuel Ordoñez on 23/06/24.
 //
 
-import Foundation
+import SwiftUI
+struct DetailItem : View {
+    @Environment(\.managedObjectContext) private var viewContext
+    let dish: Dish
+    
+    var body: some View {
+        ScrollView{
+            AsyncImage(url: URL(string: dish.image ?? "")) {
+                image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                ProgressView()
+            }
+            .clipShape(Rectangle())
+            .frame(minHeight: 150)
+            Text(dish.title ?? "")
+                .font(.subTitleFont())
+                .foregroundColor(.primaryColor1)
+            Spacer(minLength: 20)
+            Text(dish.descriptionDish ?? "")
+                .font(.regularText())
+            Spacer(minLength: 10)
+            Text("$" + (dish.price ?? ""))
+                .font(.highlightText())
+                .foregroundColor(.primaryColor1)
+                .monospaced()
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .ignoresSafeArea()
+    }
+}
+struct DetailItem_Previews: PreviewProvider{
+    static var previews: some View{
+        DetailItem(dish: PersistenceController.oneDish())
+    }
+}
